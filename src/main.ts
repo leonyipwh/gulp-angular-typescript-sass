fb708e26548b4078c1b7d25dcef177426b299d86
Leon Yip <leonyipwh@hotmail.com>
2017-02-21 03:56:44 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import * as angular from "angular";
+import 'angular-ui-router';
+
+import { AppComponent }   from "./components/app.component";
+import { HomeComponent }   from "./components/home.component";
+
+
+interface Istate {
+    name: string,
+    url: string,
+    component: string
+}
+
+module App {
+    
+	angular.module('app', ["ui.router"])
+    .config(function(
+        $stateProvider: angular.ui.IStateProvider,
+        $urlRouterProvider: angular.ui.IUrlRouterProvider,
+        $locationProvider: angular.ILocationProvider
+    ){
+        var states = [
+            {
+                name: 'home',
+                url: '/home',
+                template: '<home></home>'
+            },
+            {
+                name: 'about',
+                url: '/about',
+                template: '<about>AAABout page</about>'
+            }        
+        ];
+
+        states.forEach(function(state) {
+            $stateProvider.state(state);
+        });
+
+        $urlRouterProvider.otherwise('/home',);
+        // $locationProvider.html5Mode(true).hashPrefix("!");
+    })
+    .component(AppComponent.componentName, AppComponent.componentConfig)
+    .component(HomeComponent.componentName, HomeComponent.componentConfig)
+    ;
+}
